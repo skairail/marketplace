f6cc4d3b6dfc703783da56bc69a3114d21f6294e
Oleksandr <skairail1600@gmail.com>
2024-03-09 19:04:21 +0200

    feat : image functionality added

--- src/main/java/com/example/marketplace/controllers/ProductController.java
+++ src/main/java/com/example/marketplace/controllers/ProductController.java
@@ -8,6 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 public class ProductController {
@@ -16,7 +20,7 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
-
+    @GetMapping("/product/{id}")
     public String productInfo(@PathVariable Long id, Model model) {
         model.addAttribute("product", productService.getProductById(id));
 
@@ -24,18 +28,20 @@ public class ProductController {
     }
 
     @GetMapping("/")
-    public String products(Model model) {
-        model.addAttribute("products", productService.listProducts());
+    public String products(@RequestParam(name="title" , required = false) String title , Model model) {
+        model.addAttribute("products", productService.listProducts(title));
         return "products";
     }
 
     @PostMapping("/product/create")
-    public String createProduct(Product product){
-        productService.saveProduct(product);
+    public String createProduct(@RequestParam("file1") MultipartFile file1, @RequestParam("file2") MultipartFile file2,
+                                @RequestParam("file3") MultipartFile file3, Product product) throws IOException {
+        productService.saveProduct(product, file1, file2, file3);
         return "redirect:/";
     }
+
     @PostMapping("/product/delete/{id}")
-    public String deleteProduct(Long id){
+    public String deleteProduct(@PathVariable Long id){
         productService.deleteProduct(id);
         return "redirect:/";
 
---
+++ src/main/java/com/example/marketplace/models/Image.java
@@ -0,0 +1,28 @@
+package com.example.marketplace.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "images")
+@Data
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "originalFileName")
+    private String originalFileName;
+    @Column(name = "size")
+    private Long size;
+    @Column(name = "contentType")
+    private String contentType;
+    @Column(name = "isPreviewImage")
+    private boolean isPreviewImage;
+    @Lob
+    private byte[] bytes;
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private Product product;
+}
