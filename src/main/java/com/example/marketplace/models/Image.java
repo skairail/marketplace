e168d18a5a8f4b49432a9bda24d25c14e596c867
Oleksandr <skairail1600@gmail.com>
2024-03-10 22:11:19 +0200

    feat : users functionality added

--- src/main/java/com/example/marketplace/models/Image.java
+++ src/main/java/com/example/marketplace/models/Image.java
@@ -1,28 +1,23 @@
 package com.example.marketplace.models;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 
 @Entity
-@Table(name = "images")
 @Data
+@Table(name = "images")
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
-    @Column(name = "name")
     private String name;
-    @Column(name = "originalFileName")
     private String originalFileName;
-    @Column(name = "size")
     private Long size;
-    @Column(name = "contentType")
     private String contentType;
-    @Column(name = "isPreviewImage")
-    private boolean isPreviewImage;
+    private boolean previewImage;
     @Lob
     private byte[] bytes;
     @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
     private Product product;
+
 }
